7c90c37387e9bd68255cb4d1c04e635096d5a0b0
stephen-lakes <oluyomiolamilekan99@gmail.com>
2023-09-21 16:10:12 +0100

    unicafe step 2
    Expand application so that it shows more statistics about the gathered feedback

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,16 +1,26 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const App = () => {
   // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(6);
+  const [neutral, setNeutral] = useState(2);
+  const [bad, setBad] = useState(1);
 
   return (
     <div>
-      code here
+      <h1>give feedback</h1>
+      <button>good</button>
+      <button>neutral</button>
+      <button>bad</button>
+      <h1>statisitcs</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {good + neutral + bad}</p>
+      <p>average {((good*1) + (neutral * 0) + (bad * -1 ))/(good + neutral + bad)}</p>
+      <p>positive {good / (good + neutral + bad) * 100} %</p>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
