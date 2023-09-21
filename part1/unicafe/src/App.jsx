7f9182b83bf3a1c36af4b0a6c9794a921e5e2400
stephen-lakes <oluyomiolamilekan99@gmail.com>
2023-09-21 17:25:44 +0100

    Unicafe step 6
    Display the statistics in an HTML table
    
    Then perform the necessary actions to make the warning disappear due to
    the use of table element by nesting tbody element inside table element

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -2,11 +2,10 @@ import { useState } from "react";
 
 const StatisticLine = (props) => {
   return (
-    <div>
-      <p>
-        {props.text} {props.value}
-      </p>
-    </div>
+    <tr>
+      <td>{props.text}</td>
+      <td>{props.value}</td>
+    </tr>
   );
 };
 
@@ -27,20 +26,24 @@ const Statistics = (props) => {
     <div>
       <h1>statistics </h1>
       {good || neutral || bad ? (
-        <div>
-          <StatisticLine text="good" value={good} />
-          <StatisticLine text="neutral" value={neutral} />
-          <StatisticLine text="bad" value={bad} />
-          <StatisticLine text="all" value={good + neutral + bad} />
-          <StatisticLine
-            text="average"
-            value={(good * 1 + neutral * 0 + bad * -1) / (good + neutral + bad)}
-          />
-          <StatisticLine
-            text="positive"
-            value={(good / (good + neutral + bad)) * 100 + " %"}
-          />
-        </div>
+        <table>
+          <tbody>
+            <StatisticLine text="good" value={good} />
+            <StatisticLine text="neutral" value={neutral} />
+            <StatisticLine text="bad" value={bad} />
+            <StatisticLine text="all" value={good + neutral + bad} />
+            <StatisticLine
+              text="average"
+              value={
+                (good * 1 + neutral * 0 + bad * -1) / (good + neutral + bad)
+              }
+            />
+            <StatisticLine
+              text="positive"
+              value={(good / (good + neutral + bad)) * 100 + " %"}
+            />
+          </tbody>
+        </table>
       ) : (
         <p>No feedback given</p>
       )}
