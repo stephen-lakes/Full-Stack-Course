071a4dd089fb7933a8ae756ebe59c9a1cb1ad5ea
stephen-lakes <oluyomiolamilekan99@gmail.com>
2023-11-01 00:47:19 +0100

    create components for course information

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,14 +1,32 @@
+import Course from "./components/Course";
 import Note from "./components/Note";
 
 const App = ({ notes }) => {
+  const course = {
+    id: 1,
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+    ],
+  };
+
   return (
     <div>
-      <h1>Notes</h1>
-      <ul>
-        {notes.map((note) => (
-          <Note key={note.id} note={note} />
-        ))}
-      </ul>
+      <Course course={course} />
     </div>
   );
 };
---
+++ part2/src/components/Content.jsx
@@ -0,0 +1,21 @@
+const Part = ({ part }) => {
+  return (
+    <div>
+      <p>
+        {part.name} {part.exercises}
+      </p>
+    </div>
+  );
+};
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part) => (
+        <Part key={part.id} part={part} />
+      ))}
+    </div>
+  );
+};
+
+export default Content;
