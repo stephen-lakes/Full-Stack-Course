058561498de9b408e5e5e41afef0e58de7e8863c
stephen-lakes <oluyomiolamilekan99@gmail.com>
2023-11-01 09:03:38 +0100

    add component to render total exercises

--- part2/src/components/Course.jsx
+++ part2/src/components/Course.jsx
@@ -1,11 +1,13 @@
 import Content from "./Content";
 import Header from "./Header";
+import Total from "./Total";
 
-const Course = ({course}) => {
+const Course = ({ course }) => {
   return (
     <div>
       <Header name={course.name} />
       <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
 };
---
+++ part2/src/components/Total.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+const Total = ({ parts }) => {
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0);
+  return (
+    <div>
+      <h3>{`Total of ${total} exercises`}</h3>
+    </div>
+  );
+};
+
+export default Total;
