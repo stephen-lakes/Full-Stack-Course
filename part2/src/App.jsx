f11e0a49a9665c3629dabe84bfd0d4bc2885557d
stephen-lakes <oluyomiolamilekan99@gmail.com>
2023-11-01 10:11:45 +0100

    add states to hide/show notes based on note important status

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -3,7 +3,8 @@ import Note from "./components/Note";
 
 const App = (props) => {
   const [notes, setNotes] = useState(props.notes);
-  const [newNote, setNewNote] = useState("a new note...");
+  const [newNote, setNewNote] = useState("");
+  const [showAll, setShowAll] = useState(true);
 
   const addNote = (event) => {
     event.preventDefault();
@@ -11,10 +12,10 @@ const App = (props) => {
       content: newNote,
       important: Math.random() < 0.5,
       id: notes.length + 1,
-    }
+    };
 
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
+    setNotes(notes.concat(noteObject));
+    setNewNote("");
   };
 
   const handleNoteChange = (event) => {
@@ -22,11 +23,13 @@ const App = (props) => {
     setNewNote(event.target.value);
   };
 
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important);
+
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        {notes.map((note) => (
+        {notesToShow.map((note) => (
           <Note key={note.id} note={note} />
         ))}
       </ul>
