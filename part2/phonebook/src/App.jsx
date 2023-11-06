bf8222d224ab12b25546f52b919e1a69ac9430e4
stephen-lakes <oluyomiolamilekan99@gmail.com>
2023-11-06 11:41:41 +0100

    Note app

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -7,6 +7,27 @@ function App() {
   const [newNote, setNewNote] = useState("");
   const [showAll, setShowAll] = useState(true);
 
+  const addNote = (event) => {
+    event.preventDefault();
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
+    };
+
+    setNotes(notes.concat(noteObject));
+    setNewNote("");
+  };
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value);
+    setNewNote(event.target.value);
+  };
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter((note) => note.important === true);
+
   const hook = () => {
     console.log("effect");
     axios.get("http://localhost:3001/notes").then((response) => {
@@ -20,7 +41,21 @@ function App() {
 
   return (
     <>
-      <Note notes={notes} />
+      <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all' }
+        </button>
+      </div>
+      <ul>
+        {notesToShow.map((note) => (
+          <Note key={note.id} note={note} />
+        ))}
+      </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
     </>
   );
 }
