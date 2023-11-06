4e1fbb78c82915d0f66a2ce2b7f55c050b3fabb0
stephen-lakes <oluyomiolamilekan99@gmail.com>
2023-11-06 16:50:01 +0100

    update: Changing the importance of Notes

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -14,14 +14,10 @@ function App() {
       important: Math.random() < 0.5,
     };
 
-    axios
-      .post('http://localhost:3001/notes', noteObject)
-      .then(response => {
-        console.log(response);
-      })
-
-    // setNotes(notes.concat(noteObject));
-    // setNewNote("");
+    axios.post("http://localhost:3001/notes", noteObject).then((response) => {
+      setNotes(notes.concat(response.data));
+      setNewNote("");
+    });
   };
 
   const handleNoteChange = (event) => {
@@ -44,17 +40,31 @@ function App() {
   useEffect(hook, []);
   console.log("render", notes.length, "notes");
 
+  const toggleImportanceOf = (id) => {
+    const url = `http://localhost:3001/notes/${id}`;
+    const note = notes.find((note) => (note.id = id));
+    const changedNote = { ...note, important: !note.important };
+
+    axios.put(url, changedNote).then((response) => {
+      setNotes(notes.map((note) => (note.id !== id ? note : response.data)));
+    });
+  };
+
   return (
     <>
       <h1>Notes</h1>
       <div>
         <button onClick={() => setShowAll(!showAll)}>
-          show {showAll ? 'important' : 'all' }
+          show {showAll ? "important" : "all"}
         </button>
       </div>
       <ul>
         {notesToShow.map((note) => (
-          <Note key={note.id} note={note} />
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
         ))}
       </ul>
       <form onSubmit={addNote}>
