a89e9142520950542023a75e71ebe8e5a1691dc3
stephen-lakes <oluyomiolamilekan99@gmail.com>
2023-11-06 15:46:05 +0100

    Sending Data to the Server

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -12,11 +12,16 @@ function App() {
     const noteObject = {
       content: newNote,
       important: Math.random() < 0.5,
-      id: notes.length + 1,
     };
 
-    setNotes(notes.concat(noteObject));
-    setNewNote("");
+    axios
+      .post('http://localhost:3001/notes', noteObject)
+      .then(response => {
+        console.log(response);
+      })
+
+    // setNotes(notes.concat(noteObject));
+    // setNewNote("");
   };
 
   const handleNoteChange = (event) => {
