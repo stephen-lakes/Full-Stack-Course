5d417aee19ee5f02af53ea08bf958277470caf30
stephen-lakes <oluyomiolamilekan99@gmail.com>
2023-11-07 17:29:03 +0100

    refactor

--- part2/phonebook/src/components/Note.jsx
+++ part2/phonebook/src/components/Note.jsx
@@ -2,10 +2,12 @@ import React from "react";
 
 const Note = ({ note, toggleImportance }) => {
   const label = note.important ? "make not important" : "make important";
-  return <li>
-    {note.content}
-    <button onClick={toggleImportance}>{label}</button>
-    </li>;
+  return (
+    <li>
+      {note.content}
+      <button onClick={toggleImportance}>{label}</button>
+    </li>
+  );
 };
 
 export default Note;
