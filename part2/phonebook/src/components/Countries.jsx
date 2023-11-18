fe63fb82d57b8a4b1898229084dd37931245c77a
stephen-lakes <oluyomiolamilekan99@gmail.com>
2023-11-18 11:56:05 +0100

    refactor countries

--- part2/phonebook/src/components/Countries.jsx
+++ part2/phonebook/src/components/Countries.jsx
@@ -2,41 +2,42 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 
 const Countries = () => {
-  const [value, setValue] = useState("");
+  const [inputValue, setInputValue] = useState("");
   const [countries, setCountries] = useState([]);
+  const [filteredCountries, setFilteredCountries] = useState([]);
 
   const axiosInstance = axios.create({
     baseURL: "https://studies.cs.helsinki.fi/restcountries/api/all",
   });
 
   useEffect(() => {
-    value &&
-      axiosInstance
-        // .get(`https://studies.cs.helsinki.fi/restcountries/api/name/${country}`)
-        .get(`https://studies.cs.helsinki.fi/restcountries/api/all`)
-        .then((response) => {
-          setCountries(response.data);
-        });
-  }, [value]);
+    axiosInstance
+      // .get(`https://studies.cs.helsinki.fi/restcountries/api/name/${country}`)
+      .get(`https://studies.cs.helsinki.fi/restcountries/api/all`)
+      .then((response) => {
+        setCountries(response.data);
+      });
+  }, []);
+
+  useEffect(() => {
+    setFilteredCountries(
+      countries.filter((country) =>
+        country.name.common.toLowerCase().includes(inputValue.toLowerCase())
+      )
+    );
+  }, [inputValue]);
 
   return (
     <div>
       find countries{" "}
-      <input value={value} onChange={(e) => setValue(e.target.value)} />
+      <input
+        inputValue={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
       <div>
-        {countries.filter((country) =>
-          country.name.common.toLowerCase().includes(value.toLowerCase())
-        ).length > 10 ? (
-          <p>Too many matches, sepcify another filter</p>
-        ) : (
-          countries
-            .filter((country) =>
-              country.name.common.toLowerCase().includes(value.toLowerCase())
-            )
-            .map((country) => (
-              <p key={country.name.common}>{country.name.common}</p>
-            ))
-        )}
+        {filteredCountries.map((c) => (
+          <p key={c.name.common}>{c.name.common}</p>
+        ))}
       </div>
     </div>
   );
