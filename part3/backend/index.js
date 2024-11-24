dc5c0eac6b907e2203b8085e833f30d7d2a1a76b
stephen-lakes <oluyomiolamilekan99@gmail.com>
2024-11-24 17:05:28 +0100

    include comments above each major lines of code
    incude comment to explain how index.js file works

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,10 +1,17 @@
+// import Node's web server module
 const http = require("http");
 
+// create a new web server
+// register an event handler that is called every time an HTTP request
+// is made to the server's address http://localhost:3001
+// the request is responded to with the status code 200 and
+// set the Content-Type to text/plain and the content of the site to be returned set to Hello World
 const app = http.createServer((request, response) => {
   response.writeHead(200, { "Content-type": "text/plain" });
   response.end("Hello World");
 });
 
 const PORT = 3001;
+// bind the app variable to listen to HTTP requests sent to port 3001
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
