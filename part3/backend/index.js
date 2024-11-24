9ce0ee753512933c9045012a6d942826a265a95c
stephen-lakes <oluyomiolamilekan99@gmail.com>
2024-11-24 21:53:41 +0100

    fix: check if the notes with the id exixts
    set the repsonse status to 404 when note with the specified id does not exist

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -35,6 +35,12 @@ app.get("/api/notes", (request, response) => {
 app.get("/api/notes/:id", (request, response) => {
   const id = request.params.id;
   const note = notes.find((note) => note.id === id);
+
+  if (note) {
+    response.json(note);
+  } else {
+    response.status(404).end();
+  }
   response.json(note);
 });
 
